425c9fb3e223e8997ba1b0e5981bf4363167b50c
fizz0098 <hafizhpro00@gmail.com>
2026-06-27 01:08:56 +0700

    Initialize 8x8 board in script.js
    Menambahkan script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const board = document.getElementById("board");
+
+// Membuat papan 8x8
+for (let i = 0; i < 64; i++) {
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    board.appendChild(cell);
+}
+
+const points = document.getElementById("points");
+points.textContent = 0;
